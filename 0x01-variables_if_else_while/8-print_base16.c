843f97679d03b2d6b1eb2e0c03d1056179e878a9
Atlia-ND14 <atliadaniel14@gmail.com>
2023-09-08 11:03:34 -0700

    numberz and alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hx;
+
+	for (hx = '0'; hx <= '9'; hx++)
+	{
+		putchar(hx);
+	}
+	for (hx = 'a';hx <= 'f'; hx++)
+	{
+		putchar(hx);
+	}
+	putchar('\n');
+	return (0);
+}
