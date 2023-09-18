a947a42199419a870cb073f870f121d00a5eff43
Atlia-ND14 <atliadaniel14@gmail.com>
2023-09-18 13:58:09 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 /**
- * _strlen - returns the length of a string.
- * @s - string
+ * _strlen - returns the length of a string
+ * @s - character input
  * Return : the string len
  */
-int _strlen(char *s) 
+int _strlen(char *s)
 {
 	int len = 0;
-    
+
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
