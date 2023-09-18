f4c1d4452b011df53aeb89292c4e0131ffb27459
Atlia-ND14 <atliadaniel14@gmail.com>
2023-09-18 14:02:15 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,8 @@
 
 /**
  * _strlen - returns the length of a string
- * @s - character input
- * Return : the string len
+ * @s: character input
+ * Return: the string length
  */
 int _strlen(char *s)
 {
