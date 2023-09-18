dcf3eed2ee1233411fee93213af607414ef192d8
Atlia-ND14 <atliadaniel14@gmail.com>
2023-09-18 14:59:16 -0700

    puts and reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,13 +4,14 @@
  * @s: input to reverse
  * Return: string and in reverse
  */
-void reverse_string(char *s)
+void rev_string(char *s)
 {
 	int length = strlen(s);
-	
+
 	for (int i = 0; i < length / 2; i++)
 	{
 		char temp = s[i];
+
 		s[i] = s[length - i - 1];
 		s[length - i - 1] = temp;
 	}
