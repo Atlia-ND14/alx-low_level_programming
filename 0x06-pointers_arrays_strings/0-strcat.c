2d2a1d82a2a784a64d0273079f817f8a0c3aa9b4
Atlia-ND14 <atliadaniel14@gmail.com>
2023-09-20 23:07:08 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,23 +3,17 @@
  * _strcat - function that concatenates two strings
  * @dest: destination
  * @src: source
- * Return: dest
+ * Return: destination
  */
 char *_strcat(char *dest, char *src)
 {
-	char *temp = dest;
+	int index = 0, dest_len = 0;
 
-	while (*temp)
-	{
-		temp++;
-	}
-	while (*src)
-	{
-		*temp = *src;
-		temp++;
-		src++;
-	}
-	*temp = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
