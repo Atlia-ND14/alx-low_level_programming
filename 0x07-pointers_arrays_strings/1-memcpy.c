bac66d4514132ea86f3c47e22c795fb037a7388e
Atlia-ND14 <atliadaniel14@gmail.com>
2023-09-25 19:13:43 -0700

    memcpy strchr strspn

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: number of byte to be copied
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	if (dest == NULL || src == NULL)
+	{
+		return (0);
+	}
+	char *p = dest;
+
+	while n > 0)
+	{
+		*p = *src;
+		src++;
+		p++;
+		n--
+	}
+
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @c: character
+ * @s: string
+ * Return: a pointer or NULL
+ */
+char *_strchr(char *s, char c);
+{
+	while (*s != '/0' && *s != c)
+	{
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
