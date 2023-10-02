f061cf861455eb83566233fb12d9f14b8fe97e56
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-02 07:48:48 -0700

    static library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - function searches a string for any of a set of bytes
+ * @s: string
+ * @accept: parameter
+ * Return: pointer or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	if (s == NULL || accept == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
