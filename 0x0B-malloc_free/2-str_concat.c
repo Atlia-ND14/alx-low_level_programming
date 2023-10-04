0151f0d830275a1c59fc71c5ff3f98b7cc9a2816
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-04 09:38:17 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,23 +9,43 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
-	char *new_str = malloc(len1 + len2 + 1);
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
-	if (new_str == NULL)
-	{
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
 		return (NULL);
+
+	j = 0;
+
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
 	}
-	memcpy(new_str, s1, len1);
-	memcpy(new_str + len1, s2, len2 + 1);
-	return (new_str);
+
+	s[k] = '\0';
+	return (s);
 }
