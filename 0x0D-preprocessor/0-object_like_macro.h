d3b65c5ff28336bfc891d5dbb96a5344c77d3686
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-09 11:18:34 -0700

    malloc

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,4 @@
 #ifndef SIZE_H
 #define SIZE_H
-/**
- *@SIZE - define macro named SIZE
- */
 #define SIZE 1024
 #endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - prints the name of the file it was compiled from
+ * main - Print the name of the file the program was compiled from
+ *
  * Return: 0
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
