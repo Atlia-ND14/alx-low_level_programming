f877c521ab728e1876ced7c2b2b4422a81007261
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-12 07:14:24 -0700

    function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,12 +6,12 @@
  * @size: size of the array
  * @action: function to use on array
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
-	for (i = 0; i < size; i++;)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
