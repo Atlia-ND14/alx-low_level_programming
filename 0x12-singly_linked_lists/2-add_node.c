1094ba6cc433fd2b5d14186e0985987148da70f9
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-19 10:51:19 -0700

    updated singly linked

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,34 +6,30 @@
  * add_node - dds a new node at the end of a list_t
  * @head: head
  * @str: string
+ * Return: alwys 0
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    size_t len;
-    char *new_str;
+	list_t *new_node;
+	size_t len;
+	char *new_str;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
-
-    len = strlen(str);
-    new_str = malloc(sizeof(char) * (len + 1));
-    if (new_str == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        free(new_str);
-        return (NULL);
-    }
-
-    strcpy(new_str, str);
-    new_node->str = new_str;
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	if (head == NULL || str == NULL)
+		return (NULL);
+	len = strlen(str);
+	new_str = malloc(sizeof(char) * (len + 1));
+	if (new_str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_str);
+		return (NULL);
+	}
+	strcpy(new_str, str);
+	new_node->str = new_str;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
-
