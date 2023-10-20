464a6490463cf84caa8c71b43d308c621a83ce0e
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-20 02:24:36 -0700

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,3 +27,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *str);
 void first(void);
+#endif
