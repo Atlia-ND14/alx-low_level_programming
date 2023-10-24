65fe8c0c5c8a8a6431feb65b772490d498f00706
Atlia-ND14 <atliadaniel14@gmail.com>
2023-10-24 12:38:48 -0700

    more sigle linked

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
