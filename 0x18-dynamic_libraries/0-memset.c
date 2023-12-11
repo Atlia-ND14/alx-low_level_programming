0d607c214db3e4bccb3f5928bd83747de960ae20
Atlia-ND14 <atliadaniel14@gmail.com>
2023-12-11 01:05:05 -0800

    dynamic library

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: memory area
+ * @b: constant
+ * @n: no of bytrs to be filled
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n > 0)
+	{
+		*p = b;
+		p++;
+		n--;
+	}
+
+	return (s);
+}
